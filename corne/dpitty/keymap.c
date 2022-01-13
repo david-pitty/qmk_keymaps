20ae4e9afb3670b34eb45db9616d1ed4355ada01
David Pitty <dpitty13@gmail.com>
2022-01-12 21:05:23 -0600

    added shift to number layer

--- corne/dpitty/keymap.c
+++ corne/dpitty/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,                        KC_7,    KC_8,    KC_9,    KC_0,  KC_LBRC, KC_RBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_HOME, KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT,  KC_END,
+      KC_LCTL, _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LSFT,                      KC_HOME, KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT,  KC_END,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_LSFT,   MO(3), KC_LALT
                                       //`--------------------------'  `--------------------------'
