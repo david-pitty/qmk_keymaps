9e5b52af83e171f270ede5f353e27fb469582bda
david-pitty <dpitty13@gmail.com>
2022-01-14 19:46:04 -0600

    enable rgb

--- corne/dpitty/rules.mk
+++ corne/dpitty/rules.mk
@@ -1,2 +1,6 @@
+MOUSEKEY_ENABLE = no
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+LTO_ENABLE          = yes
