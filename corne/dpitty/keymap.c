b64591f86f1507f267de570d9288446cf9f7e514
david-pitty <dpitty13@gmail.com>
2022-05-31 08:33:44 -0600

    move backspace in plover layer to include windows key

--- corne/dpitty/keymap.c
+++ corne/dpitty/keymap.c
@@ -76,13 +76,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [4] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      EXT_PLV,  KC_GRV,  KC_GRV,  KC_GRV,  KC_GRV,  KC_GRV,                       KC_GRV,  KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, KC_GRV,
+      EXT_PLV,  KC_GRV,  KC_GRV,  KC_GRV,  KC_GRV,  KC_GRV,                       KC_GRV,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,  KC_GRV,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BSPC,    KC_C,    KC_V,       KC_N,    KC_M,  KC_LALT
+                                          KC_LGUI,    KC_C,    KC_V,       KC_N,    KC_M,  KC_LALT
                                       //`--------------------------'  `--------------------------'
   )
 };
