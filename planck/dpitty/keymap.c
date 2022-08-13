74f914ff6439748008c05d6f3cf4b5be67c7af75
david-pitty <dpitty13@gmail.com>
2022-08-13 16:10:37 -0600

    changed function keys in plover layout

--- planck/dpitty/keymap.c
+++ planck/dpitty/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   S  |   K  |   W  |   R  |   *  |      |   *  |   R  |   B  |   G  |   S  |   Z  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |Enter |
+ * | Shift|   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |  Tab | Bksp |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl |  ALT | GUI  |   A  |   O  |      E      |   U  | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_PLOVER] = LAYOUT_planck_grid(
     KC_GRV,  KC_W,    KC_E,    KC_R,    KC_T,    EXT_PLV, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    XXXXXXX, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TAB,
+    KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_BSPC, KC_TAB,  KC_ENT,
     KC_LCTL, KC_LALT, KC_LGUI, KC_C,    KC_V,    KC_N,    KC_N,    KC_M,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
